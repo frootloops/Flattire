4589c9f2003883c8c5010d2198fee94ffbbba7f8
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-02-21 21:08:46 +0300

    Add UserDefaults lib

--- Flattire/AppDelegate.swift
+++ Flattire/AppDelegate.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import SwiftyUserDefaults
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        changeStoryboardIfNeeded()
         return true
     }
 
@@ -39,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // MARK: Private
+    
+    private func changeStoryboardIfNeeded() {
+        guard !Defaults[.hasLaunchedOnce] else { return }
+        Defaults[.hasLaunchedOnce] = true
+        
+        let welcome = UIStoryboard(name: "Welcome", bundle: .mainBundle())
+        window?.rootViewController = welcome.instantiateInitialViewController()
     }
 
 
---
+++ Flattire/UserDefaults.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaults.swift
+//  Flattire
+//
+//  Created by Arsen Gasparyan on 21/02/16.
+//  Copyright © 2016 Arsen Gasparyan. All rights reserved.
+//
+
+import Foundation
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let hasLaunchedOnce = DefaultsKey<Bool>("hasLaunchedOnce")
+}
